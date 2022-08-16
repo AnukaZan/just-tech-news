1bb80013c3e31e70a6efe0b8592fbd949e77facb
Anuka <anukazan@gmail.com>
2022-08-15 18:50:24 -0700

    add post routes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,16 @@
 const User = require('./User');
 const Post = require('./Post');
 
+//id in User to link to user_id in Post 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
 module.exports = { User, Post };
 
 //importing User model and exporting an object with it as a property 
